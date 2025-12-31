b255dcd9907ec22e17b6c537fe2d408d8eea4db8
Elansil <andrzej6453@gmail.com>
2025-12-31 14:43:04 +0100

    zmiana flag na flagi bitowe

--- LZ77/Source.cpp
+++ LZ77/Source.cpp
@@ -15,28 +15,45 @@ void AlgorytmKodujacyLZ77(const std::string& ipath, const std::string& opath)
 	std::ofstream ofile(opath, std::ios::binary);
 	if(ifile.good() && ofile.good())
 	{
-		const char flaga_l = -1;
-		const char flaga_p = -2;
 		auto size = ifile.tellg();
 		ifile.seekg(0);
 		fullText.resize(size);
 		ifile.read(&fullText[0], size);
+
+		long long pos = ofile.tellp();
+		unsigned char flagBajt{};
+		ofile.put(flagBajt);		//place holder
+		int bit_count{};
+
 		for (int cursor1 = 0; cursor1 < size; cursor1++)
 		{
 			unsigned short dystans{};
 			unsigned char dlugosc{};
-			int max{0};
+			unsigned short max{};
+			if (bit_count == 8)
+			{
+				long long temp_pos = ofile.tellp();
+				ofile.seekp(pos);
+				ofile.put(flagBajt);
+				ofile.seekp(temp_pos);
+				pos = temp_pos;
+				flagBajt = 0;
+				bit_count = 0;
+				ofile.put(flagBajt);	//place holder
+			}
 			for (int cursor2 = 0; cursor2 < cursor1; cursor2++)
 			{
-				int count{};
+				unsigned short count{};
 				if(fullText[cursor2] == fullText[cursor1])
 				{
 					int temp_cursor1 = cursor1;
 					int temp_cursor2 = cursor2;
-					while (fullText[temp_cursor1++] == fullText[temp_cursor2++])
+					while (fullText[temp_cursor1++] == fullText[temp_cursor2++] && temp_cursor2<cursor1-1)
 						count++;
 					if(count > max)
 					{
+						if (count > 255)
+							count = 255;
 						max = count;
 						dlugosc = max;
 						dystans = cursor1 - cursor2;
@@ -45,17 +62,30 @@ void AlgorytmKodujacyLZ77(const std::string& ipath, const std::string& opath)
 			}
 			if (max<3)
 			{
-				ofile.put(flaga_l);
+				flagBajt <<= 1;
+				bit_count++;
 				ofile.put(fullText[cursor1]);
 			}
 			else if(max>=3)
 			{
+				flagBajt <<= 1;
+				flagBajt |= 1;
+				bit_count++;
 				cursor1 += dlugosc-1;
-				ofile.put(flaga_p);
 				ofile.write(reinterpret_cast<const char*>(&dystans), sizeof(dystans));
 				ofile.write(reinterpret_cast<const char*>(&dlugosc), sizeof(dlugosc));
 				max = 0;
 			}
+		}
+		if (bit_count != 8)		//warunek zakonczenia programu
+		{
+			flagBajt <<= 1;
+			flagBajt |= 128;
+			ofile.put(flagBajt);
+			unsigned short dystans{};
+			unsigned char dlugosc{};
+			ofile.write(reinterpret_cast<const char*>(&dystans), sizeof(dystans));
+			ofile.write(reinterpret_cast<const char*>(&dlugosc), sizeof(dlugosc));
 		}
 	}
 	else
@@ -77,29 +107,43 @@ void AlgorytmDekodujacyLZ77(const std::string& ipath, const std::string& opath)
 		fullText.resize(size);
 		ifile.read(&fullText[0], size);
 
-		const char flaga_l = -1;
-		const char flaga_p = -2;
+		unsigned char flagBajt{};
+		int bit_count = 8;
 
 		std::string dekoded;
 		int cursor = 0;
 		while(cursor < size)
 		{
-			if (fullText[cursor] == flaga_l)
+			if (bit_count == 8)
 			{
+				bit_count = 0;
+				flagBajt = fullText[cursor];
 				cursor++;
+			}
+			bool bit = (flagBajt & 128)!=0;
+			flagBajt <<= 1;
+			bit_count++;
+			if (bit == 0)
+			{
 				dekoded.push_back(fullText[cursor]);
 				ofile.put(fullText[cursor]);
-				cursor++;
+				cursor+=sizeof(unsigned char);
 				continue;
 			}
-			if (fullText[cursor] == flaga_p)
+			else if (bit == 1)
 			{
-				cursor++;
 				unsigned short dystans = *reinterpret_cast<const unsigned short*>(&fullText[cursor]);
 				cursor += sizeof(unsigned short);
 				unsigned char dlugosc = *reinterpret_cast<const unsigned char*>(&fullText[cursor]);
 				cursor += sizeof(unsigned char);
 
+				if (dystans == 0 && dlugosc == 0)		//warunek zakonczenia programu
+					break;
+				if (dystans > dekoded.size())
+				{
+					std::cout << "Blad odczytu danych" << std::endl;
+					return;
+				}
 				int index = dekoded.size() - dystans;
 				for (int i = 0; i < dlugosc; i++)
 				{
